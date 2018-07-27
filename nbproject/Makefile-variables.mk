3130291c8d5b439a22b1a88ab4e444967a2c3145
Coding96 <Edward.Collins@live.co.uk>
2018-07-27 10:30:41 +0100

    inital

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=ncurses-fun
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/ncurses-fun
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=ncurses-fun.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/ncurses-fun.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=ncurses-fun
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/ncurses-fun
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=ncurses-fun.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/ncurses-fun.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
